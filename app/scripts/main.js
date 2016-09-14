a4b9293ce6ea7ff3b850c252c2b8a02df67d462a
Richard Roncancio <batusai513@msn.com>
2016-09-13 21:22:37 -0500

    add mobile styles to the main menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,22 @@
-var shuffle = new shuffle(document.querySelector('.destacados'), {
-  itemSelector: '.destacado'
+// prefixer helper function
+var pfx = ["webkit", "moz", "MS", "o", ""];
+function prefixedEventListener(element, type, callback) {
+    for (var p = 0; p < pfx.length; p++) {
+        if (!pfx[p]) type = type.toLowerCase();
+        element.addEventListener(pfx[p]+type, callback, false);
+    }
+}
+
+// new event listener function
+/*var monkey = document.querySelector("#monkey");
+prefixedEventListener(monkey,"AnimationStart",function(e){
+    console.log("log at beginning of monkey animation");
+});*/
+
+$(function(){
+
 });
+
+  var shuffle = new shuffle(document.querySelector('.destacados'), {
+    itemSelector: '.destacado'
+  });
